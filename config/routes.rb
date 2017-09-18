917a27dbed65ba164462b17907ff390078023be8
megancallenbach <megancallenbach@gmail.com>
2017-09-18 11:23:08 +0200

    Add models, controllers, routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users do
+    resources :admins
+    resources :students
+  end
+  resources :pairs
+
 end
