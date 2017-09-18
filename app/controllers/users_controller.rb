608466fe2e698ef4608ef3f969cb25d9cfe2b4f9
megancallenbach <megancallenbach@gmail.com>
2017-09-18 16:08:16 +0200

    Change Admin Role of Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,24 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "Admin status was updated"
+    else
+      render :edit
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:admin)
+  end
 end
