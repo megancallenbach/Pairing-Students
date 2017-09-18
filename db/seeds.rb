5017657b129069d8e1aa9597af9925b79224930b
MimiMagusin <mimi_magusin@hotmail.com>
2017-09-18 12:14:40 +0200

    Set up user column add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+admin1 = User.create(email: "admin@pairingstudents.com", password: "12345678", admin: true)
