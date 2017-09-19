d2030ab476c580a452664d87e36e377c104a3e80
MimiMagusin <mimi_magusin@hotmail.com>
2017-09-19 10:34:57 +0200

    Create pair in method

--- app/models/pair.rb
+++ app/models/pair.rb
@@ -1,22 +1,6 @@
 class Pair < ApplicationRecord
   has_many :users
 
-  def create_pair
-    @lonely_students = ["Megan", "Mimi", "Bob", "Charles", "Lila", "Lola"]
-    @students = @users
-    user1 = @students[rand(0..((@students.length) - 1))]
-
-    @students.delete(user1)
-
-    user2 = @students[rand(0..((@students.length) -1))]
-
-    @students.delete(user2)
-
-    @pair = [user1, user2]
-
-    return @pair
-
-  end
 
   def create_pairs_today
 		@history = []
@@ -31,7 +15,7 @@ class Pair < ApplicationRecord
 			user2 = @lonely_students[rand(0..((@lonely_students.length) -1))]
 
 			@lonely_students.delete(user2)
-      # @pair = Pair.create!
+      @pair = Pair.create!
 			@pair = [user1, user2]
 
 			if @history.include? @pair || @pair.reverse
