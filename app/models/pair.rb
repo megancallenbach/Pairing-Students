05933388919b4dca93668e8435825f3e9a53866c
MimiMagusin <mimi_magusin@hotmail.com>
2017-09-21 11:41:21 +0200

    need to commit to switch branch

--- app/models/pair.rb
+++ app/models/pair.rb
@@ -8,42 +8,50 @@ class Pair < ApplicationRecord
   end
   @@pairs_today = []
 
-  def self.weekly_pairs
-    i = 0
-    while i < 6
-      day = Date.today - i
-      create_pairs(day)
-      i += 1
-    end
-  end
+  # def self.weekly_pairs
+  #   i = 0
+  #   while i < 6
+  #     day = Date.today - i
+  #     create_pairs(day)
+  #     i += 1
+  #   end
+  # end
 
 
-  def self.create_pairs(day)
+  def self.create_pairs()
     i = 0
-    puts "Lonely: #{@@lonely_students}"
+
+    #roundrobin array each time function is called
     special_student = @@lonely_students.shift
-    puts "Special student #{special_student}"
     @@lonely_students.rotate!
-    puts "Lonely: #{@@lonely_students}"
     @@lonely_students.unshift(special_student)
+
     while i < (@@lonely_students.length/2)
-      @new_pair = Pair.create!
+      #pick two students from array
       student1 = @@lonely_students[i]
       student2 = @@lonely_students[i + (@@lonely_students.length / 2)]
+
+      #add students pair attribute
+      @new_pair = Pair.create!
       @new_pair.update(student1: student1)
       @new_pair.update(student2: student2)
-      @new_pair.update(date: day)
+      #@new_pair.update(date: day)
+
+      #add students to pair array
       @pair = []
-      puts "Lonely: #{@@lonely_students}"
       @pair << student1
       @pair << student2
       @@pairs_today << @pair
+
       i += 1
 
+      #update pair_id for user1
       first_name1 = student1.split(' ')[0]
       last_name1 = student1.split(' ')[1]
       student_1 = User.find_by(first_name: first_name1, last_name: last_name1)
       student_1.update(pair_id: @new_pair.id)
+
+      #update pair_id for user2
       first_name2 = student2.split(' ')[0]
       last_name2 = student2.split(' ')[1]
       student_2 = User.find_by(first_name: first_name2, last_name: last_name2)
