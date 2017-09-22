5fc6162dfe816c39ab7ce61d13b0a3f3ec791560
MimiMagusin <mimi_magusin@hotmail.com>
2017-09-22 11:08:42 +0200

    Update routes, fix profile problems, update test admin

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   devise_for :users
-  resources :users
-  # do
-  #   resources :admins
-  #   resources :students
-  # end
-  resources :pairs
-  resources :profiles, only: [:new, :edit, :create, :update]
+  resources :users, except: [:destroy]
+  resources :pairs, except: [:destroy]
+
 end
